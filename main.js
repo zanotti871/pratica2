8e0724dbb76aa58369582f99b1beac731a806c4b
lbc <gringo871@hotmail.com>
2023-05-28 17:42:14 -0300

    practico2

--- main.js
+++ main.js
@@ -1,128 +1,136 @@
-// Objeto con los productos y sus precios
+class Zapatillas {
+  constructor(id, marca, precio, talle) {
+    this.id = id;
+    this.marca = marca;
+    this.precio = precio;
+    this.talle = talle;
+  }
+}
+
 let productos = [
-  { nombre: "leche", precio: 250.0 },
-  { nombre: "pan", precio: 700.3 },
-  { nombre: "arroz", precio: 180.0 },
-  { nombre: "fideos", precio: 258.0 },
+  { id: 1, marca: "Nike", precio: 25000 },
+  { id: 2, marca: "Adidas", precio: 45000 },
+  { id: 3, marca: "Puma", precio: 20000 },
+  { id: 4, marca: "Reeboock", precio: 25000 },
 ];
 
-let costoTotal = 0;
-let carrito = [];
-
-let nombreUsuario = prompt(
-  "¡Bienvenido  a tu supermercado Virtual!\nPor favor, ingrese su nombre:"
-).toLowerCase();
-
-if (nombreUsuario) {
-  alert(`Hola ${nombreUsuario}! Bienvenido al supermercado virtual.`);
-} else {
-  alert("¡Hola! Bienvenido al supermercado virtual.");
-}
-
-while (true) {
-  let opcionesProductos = "";
-  for (let i = 0; i < productos.length; i++) {
-    opcionesProductos += `${i + 1}. ${productos[i].nombre} - $${
-      productos[i].precio
-    }\n`;
-  }
-
-  let productoElegido = parseInt(
-    prompt(
-      `Seleccione un producto de la lista:\n${opcionesProductos}\n(Ingrese el número correspondiente)\n(Presione Cancelar para finalizar la selección)`
-    )
-  );
-  if (isNaN(productoElegido)) {
-    if (carrito.length === 0) {
-      alert("No ha seleccionado ningún producto. El proceso se finalizará.");
-      break;
-    } else {
-      let finalizarCompra = confirm("¿Desea finalizar la compra?");
-      if (finalizarCompra) {
-        break;
-      } else {
-        continue;
-      }
-    }
-  }
-
-  if (productoElegido >= 1 && productoElegido <= productos.length) {
-    let cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar:"));
-
-    // Obtener el producto seleccionado
-    let productoSeleccionado = productos[productoElegido - 1];
-
-    // Calcular el costo total del producto
-    let costoProducto = productoSeleccionado.precio * cantidad;
-
-    // Agregar el producto al carrito
-    carrito.push({
-      nombre: productoSeleccionado.nombre,
-      cantidad: cantidad,
-      costo: costoProducto,
-    });
-
-    costoTotal += costoProducto;
-  } else {
+function mostrarProducto(id) {
+  const producto = productos.find((p) => p.id === id);
+  if (producto) {
     alert(
-      "Producto no válido. Por favor, seleccione un número de producto válido."
+      `Producto seleccionado:\n\nID: ${producto.id}\nMarca: ${producto.marca}\nPrecio: ${producto.precio}`
     );
+  } else {
+    alert("No se encontró el producto con el ID ingresado");
   }
+}
+// función cargar datos del producto
+function cargarProducto() {
+  let id = parseInt(prompt("Ingrese el ID del producto"));
+  let marca = prompt("Ingrese la marca del producto");
+  let precio = parseInt(prompt("Ingrese el precio"));
+  let talle = prompt("Ingrese el talle");
+  const nuevoProducto = new Zapatillas(id, marca, precio, talle);
+  nuevoProducto.cantidad = parseInt(prompt("Ingrese la cantidad de productos"));
+  arrayCarrito.push(nuevoProducto);
+  alert("Producto agregado al carrito");
+}
+// función nuestra la oferta
+function mostrarOferta() {
+  let mensaje = "Ofertas disponibles:\n\n";
+  productos.forEach((producto) => {
+    mensaje += `ID: ${producto.id}\nMarca: ${producto.marca}\nPrecio: ${producto.precio}\n\n`;
+  });
+  alert(mensaje);
+}
 
-  let seguirComprando = confirm("¿Desea seguir comprando?");
-  if (!seguirComprando) {
-    let finalizarCompra = confirm("¿Desea finalizar la compra?");
-    if (finalizarCompra) {
-      break;
-    }
+function verCarrito() {
+  if (arrayCarrito.length === 0) {
+    alert("El carrito está vacío");
+  } else {
+    let mensaje = "Productos en el carrito:\n\n";
+    arrayCarrito.forEach((producto) => {
+      mensaje += `Marca: ${producto.marca}\nTalle: ${producto.talle}\nPrecio: ${producto.precio}\nCantidad: ${producto.cantidad}\n\n`;
+    });
+    alert(mensaje);
   }
 }
 
-if (carrito.length > 0) {
-  // Imprimir los productos seleccionados en el carrito
-  console.log("Productos seleccionados:");
-  for (let i = 0; i < carrito.length; i++) {
-    console.log(
-      `- ${carrito[i].cantidad} ${carrito[i].nombre}: $${carrito[
-        i
-      ].costo.toFixed(2)}`
-    );
-  }
+function finalizarCompra() {
+  let total = 0;
+  let mensaje = "Detalles de la compra:\n\n";
+  arrayCarrito.forEach((producto) => {
+    const subtotal = producto.precio * producto.cantidad;
+    mensaje += `Producto: ${producto.marca}\nUnidades: ${producto.cantidad}\nPrecio unitario: ${producto.precio}\nSubtotal: ${subtotal}\n\n`;
+    total += subtotal;
+  });
+  const totalConIVA = total * 1.21; // Aplicando un IVA del 21%
 
-  // Forma de pago
   let formaPago = prompt(
-    "Seleccione una forma de pago:\n1. Transferencia (10% de descuento)\n2. Tarjeta de crédito (Visa o Mastercard, hasta 3 pagos sin interés)"
+    "Seleccione la forma de pago:\n1. Transferencia bancaria\n2. Tarjeta de crédito"
   );
 
-  let costoFinal = costoTotal;
+  let mensajePago = "Forma de pago seleccionada: ";
+  let montoFinal = 0;
 
-  switch (formaPago) {
-    case "1": // Transferencia
-      costoFinal *= 0.9; // Aplicar descuento del 10%
-      break;
-    case "2": // Tarjeta de crédito
-      let cuotas = parseInt(prompt("Ingrese la cantidad de cuotas (hasta 3):"));
-      if (cuotas > 3) {
-        cuotas = 3;
-      }
-      costoFinal /= cuotas; // Dividir el costo total en cuotas sin interés
-      break;
-    default:
-      alert(
-        "Forma de pago no válida. Se calculará el costo sin descuentos ni cuotas."
-      );
+  if (formaPago === "1") {
+    mensajePago += "Transferencia bancaria\n";
+    montoFinal = totalConIVA;
+  } else if (formaPago === "2") {
+    mensajePago += "Tarjeta de crédito\n";
+    let cuotas = parseInt(
+      prompt("Seleccione la cantidad de cuotas (1, 2 o 3)")
+    );
+    if (cuotas >= 1 && cuotas <= 3) {
+      montoFinal = totalConIVA / cuotas;
+      mensajePago += `Cuotas: ${cuotas}\n`;
+    } else {
+      mensajePago += "Cantidad de cuotas no válida\n";
+      montoFinal = totalConIVA;
+    }
+  } else {
+    mensajePago += "Forma de pago no válida\n";
+    montoFinal = totalConIVA;
   }
 
-  // Calcular el IVA
-  const iva = 0.21;
-  costoFinal *= 1 + iva; // Aplicar el 21% de IVA
+  mensaje += `Total a pagar (IVA incluido): ${totalConIVA}\n`;
+  mensaje += `Monto a pagar por cuota: ${montoFinal}\n\n`;
+  mensaje += mensajePago;
 
-  alert(`El costo total de su compra es: $${costoTotal.toFixed(2)}`);
-  alert(
-    `El costo final a pagar (incluyendo el 21% de IVA) es: $${costoFinal.toFixed(
-      2
-    )}`
+  alert(mensaje);
+}
+
+let arrayCarrito = [];
+
+let nombreUsuario = prompt("Ingrese su nombre");
+alert(`¡Bienvenido/a, ${nombreUsuario}!`);
+
+mostrarOferta();
+
+let opcion = prompt(
+  "Ingrese una opción: \n 1: Mostrar producto \n 2: Cargar producto \n 3: Mostrar oferta \n 4: Ver carrito \n 5: Finalizar compra \n 6: Salir"
+);
+
+while (opcion !== "6") {
+  if (opcion === "1") {
+    let id = parseInt(prompt("Ingrese el ID del producto a mostrar"));
+    mostrarProducto(id);
+  }
+  if (opcion === "2") {
+    cargarProducto();
+  }
+  if (opcion === "3") {
+    mostrarOferta();
+  }
+  if (opcion === "4") {
+    verCarrito();
+  }
+  if (opcion === "5") {
+    finalizarCompra();
+  }
+  opcion = prompt(
+    "Vuelva a ingresar una opción: \n 1: Mostrar producto \n 2: Cargar producto \n 3: Mostrar oferta \n 4: Ver carrito \n 5: Finalizar compra \n 6: Salir"
   );
 }
 
-alert("¡Gracias por utilizar por compra! ¡Hasta la próxima!");
+alert("¡Hasta pronto! Gracias por visitarnos.");
